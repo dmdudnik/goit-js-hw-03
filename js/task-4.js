bf4630fa9440d6a932fbecf71d71d95d95691f06
Dmitriy Dudnik <ddudnik79@gmail.com>
2020-12-14 21:23:41 +0200

    сделано задание 4

---
+++ js/task-4.js
@@ -0,0 +1,16 @@
+const countTotalSalary = function (employees) {
+  let sum = 0;
+  const salariesValues = Object.values(employees);
+
+  for (const salary of salariesValues) {
+    sum += salary;
+  }
+
+  return sum;
+};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 })); // 330
+
+console.log(countTotalSalary({ kiwi: 200, lux: 50, chelsy: 150 })); // 400
